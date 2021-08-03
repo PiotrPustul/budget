88d3e1a18efab3f7aea7749d37c585f970e88d4e
Piotr_Pustul <piotrpustul1@gmail.com>
2021-08-03 14:32:13 +0100

    created Navigation component, added normalize styles into Navigation component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Wrapper } from 'components';
+
+const Container = styled.div`
+   background-color: ${({ theme }) => theme.colors.gray.light};
+   padding: ${({ theme }) => theme.spacing.sm}px 0;
+   display: flex;
+   justify-content: space-between;
+`;
+
+const List = styled.ul`
+   display:flex;
+`;
+
+const Navigation = ({ items }) => {
+   return (
+      <Container>
+         <Wrapper>
+            <List>
+               {items.map(item => (
+                  <li>
+                     <a>{item.content}</a>
+                  </li>
+               ))}
+            </List>
+         </Wrapper>
+      </Container>
+   )
+};
+
+export default Navigation;
